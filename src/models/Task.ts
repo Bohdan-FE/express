c13c689aab2d019a218050550430af4567aab7e1
Bohdan <bohdan.vivchar@qorpo.world>
2025-09-28 20:40:54 +0200

    Update Task.ts

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,55 +1,68 @@
-import  { model, Schema, Types } from "mongoose";
+import { model, Schema, Types } from "mongoose";
 import { handleSaveError } from "./hooks";
-import Joi, { date } from "joi";
+import Joi from "joi";
 
-const taskSchema = new Schema({
-    description: {
+const taskSchema = new Schema(
+  {
+    title: {
         type: String,
-        required: [true, 'Description is required'],
+        require: [true, 'Title is required']
+    },
+    description: {
+      type: String,
     },
     index: {
-        type: Number,
-        required: [true, 'Index is required'],
+      type: Number,
+      required: [true, "Index is required"],
     },
-    done: {
-        type: Boolean,
-        default: false,
+    status: {
+      type: String,
+      enum: ["todo", "in_progress", "done"],
+      default: "todo",
+      required: true,
     },
     date: {
-        type: Date,
-        required: [true, 'Date is required'],
+      type: Date,
+      required: [true, "Date is required"],
     },
     owner: {
-        type: Types.ObjectId, 
-        ref: 'user',  
-        required: true,     
-      }
-}, { versionKey: false, timestamps: true })
+      type: Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-taskSchema.post('save', handleSaveError)
+taskSchema.post("save", handleSaveError);
 
-const Task = model('task', taskSchema)
+const Task = model("task", taskSchema);
 
 export const createTaskSchema = Joi.object({
-    description: Joi.string().required(),
-    index: Joi.number().required(),
-    date: Joi.date().required(),
-})
+  title: Joi.string().required(),
+  description: Joi.string(),
+  index: Joi.number().required(),
+  date: Joi.date().required(),
+  status: Joi.string().valid("todo", "in_progress", "done").default("todo"),
+});
 
 export const updateTaskSchema = Joi.object({
-    description: Joi.string(),
-    index: Joi.number(),
-    done: Joi.boolean(),
-    date: Joi.date(),
-}).or('description', 'index', 'done', 'date')
+title: Joi.string(),
+  description: Joi.string(),
+  index: Joi.number(),
+  date: Joi.date(),
+  status: Joi.string().valid("todo", "in_progress", "done"),
+}).or("description", "index", "date", "status");
 
 export const reorderTasksSchema = Joi.object({
-    tasks: Joi.array().items(
+  tasks: Joi.array()
+    .items(
       Joi.object({
         taskId: Joi.string().required(),
         index: Joi.number().required(),
       }).required()
-    ).required()
-  });
+    )
+    .required(),
+});
 
-export default Task
+export default Task;
