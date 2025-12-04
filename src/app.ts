64d34ff7f9ab28b2b1cd4da2b39afb7365f5923e
Bohdan <bohdan.vivchar@qorpo.world>
2025-12-04 21:19:00 +0100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -22,8 +22,11 @@ app.use(
       'https://stenohaline-cuc-unimposing.ngrok-free.dev',
     ],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
   }),
 );
+app.options('*', cors());
 app.use(express.json());
 app.use(express.static('public'));
 
